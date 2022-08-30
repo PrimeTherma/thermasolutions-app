0cc7e834386f90397dd46d9132c313724b21ca54
Tim Miner <poeminminer1@gmail.com>
2022-08-30 15:25:01 -0500

    added history.push which will switch to the chart page on button click.

--- src/components/StartProcedurePage/StartProcedurePage.jsx
+++ src/components/StartProcedurePage/StartProcedurePage.jsx
@@ -3,9 +3,10 @@ import { useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from 'react-redux';
 
 
+
 function StartProcedurePage() {
   const dispatch = useDispatch();
-
+  const history = useHistory();
 
   const startProcedure = (event) => {
     event.preventDefault();
@@ -13,6 +14,8 @@ function StartProcedurePage() {
       // no payload since we only need user.id and can get that directly from the passport. 
       type: 'ADD_NEW_PROCEDURE'
     })
+    history.push('/chart');
+
   }
 
   return (
