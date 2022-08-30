97e37c4b1f80c29676a4152aafd58c7f1f6ca4e9
Diego Baeza <diegob12@live.com>
2022-08-30 13:54:10 -0500

    add components and routes

---
+++ src/components/HistoryPage/HistoryPage.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+function HistoryPage() {
+  return (
+    <div className="container">
+      <div className="grid">
+        <button>Diagnostics</button>
+      </div>
+    </div>
+  );
+}
+
+export default HistoryPage;
