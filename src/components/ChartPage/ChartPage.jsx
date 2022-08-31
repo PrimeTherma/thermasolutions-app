1ef5c3acf9740d37aa1196c36438b1a964a72ea3
Mahmoud Mahmoud <99694897+Mahmooodyy@users.noreply.github.com>
2022-08-31 09:49:46 -0500

    finished creating stop watch, and is functioning

--- src/components/ChartPage/ChartPage.jsx
+++ src/components/ChartPage/ChartPage.jsx
@@ -1,11 +1,19 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+// import React, { Component } from "react";
+import "./ChartPage.css";
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 import { useHistory } from "react-router-dom";
+import Stopwatch from "../Stopwatch/Stopwatch";
+// import Stopwatch from "./Stopwatch";
 
 function ChartPage() {
   return (
     <div className="container">
       <div className="grid">
         <h3>Total HTUs: </h3>
+        <div id = "box"/>
+        <Stopwatch />
       </div>
     </div>
   );
