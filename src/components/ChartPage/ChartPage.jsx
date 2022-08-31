c1b4aca19d0588f83f7624be6049c8adc88e0d2b
Diego Baeza <diegob12@live.com>
2022-08-31 12:13:59 -0500

    remove redundant lines

--- src/components/ChartPage/ChartPage.jsx
+++ src/components/ChartPage/ChartPage.jsx
@@ -1,11 +1,8 @@
 // import React, { useState } from "react";
 // import React, { Component } from "react";
 import "./ChartPage.css";
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import { useHistory } from "react-router-dom";
 import Stopwatch from "../Stopwatch/Stopwatch";
-// import Stopwatch from "./Stopwatch";
 import Chart from "../Chart/Chart"
 
 function ChartPage() {
