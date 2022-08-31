439ff38b2d2286b548efa55ce8c4d0c81c19fb7b
Diego Baeza <diegob12@live.com>
2022-08-31 12:12:28 -0500

    add ternary to show diagnostics table, create function to fetch diagnostics

--- src/components/HistoryPage/HistoryPage.jsx
+++ src/components/HistoryPage/HistoryPage.jsx
@@ -1,12 +1,40 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 
 function HistoryPage() {
-  return (
+
+  const dispatch = useDispatch();
+
+  // state for return - ternary
+  const [show, setShow] = useState(false);
+
+  // Gets diagnostics
+  const getDiagnostics = () =>  {
+    console.log('in getDiagnostics');
+
+    // Shows diagnostics table
+    setShow(true);
+
+    dispatch({type: "FETCH_DIAGNOSTICS"});
+  }
+
+  const hideDiagnostics = () => {
+    console.log('in hideDiagnostics');
+
+    // Hides diagnostics table
+    setShow(false);
+  }
+
+  return !show ? (
     <div className="container">
       <div className="grid">
-        <button>Diagnostics</button>
+        <button onClick={getDiagnostics}>Diagnostics</button>
       </div>
     </div>
+  ) : (
+    <>
+      <button onClick={hideDiagnostics}>Hide</button>
+    </>
   );
 }
 
