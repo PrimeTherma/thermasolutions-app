e86ef134e4d1f6e30b507f4c7817c60cc83698b2
Diego Baeza <diegob12@live.com>
2022-09-01 10:25:02 -0500

    create MUI table, code for diagnostics GET

--- src/components/HistoryPage/HistoryPage.jsx
+++ src/components/HistoryPage/HistoryPage.jsx
@@ -2,8 +2,17 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import useReduxStore from "../../hooks/useReduxStore";
 
-function HistoryPage() {
+// MUI Styling
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
 
+function HistoryPage() {
   const store = useReduxStore();
   const dispatch = useDispatch();
 
@@ -11,31 +20,75 @@ function HistoryPage() {
   const [show, setShow] = useState(false);
 
   // Gets diagnostics
-  const getDiagnostics = () =>  {
-    console.log('in getDiagnostics');
+  const getDiagnostics = () => {
+    console.log("in getDiagnostics");
 
     // Shows diagnostics table
     setShow(true);
 
-    dispatch({type: "FETCH_DIAGNOSTICS"});
-  }
+    dispatch({ type: "FETCH_DIAGNOSTICS" });
+  };
 
   const hideDiagnostics = () => {
-    console.log('in hideDiagnostics');
+    console.log("in hideDiagnostics");
 
     // Hides diagnostics table
     setShow(false);
-  }
+  };
 
   return !show ? (
     <div className="container">
       <div className="grid">
-        <button onClick={getDiagnostics}>Diagnostics</button>
+        <Button onClick={getDiagnostics}>Diagnostics</Button>
       </div>
     </div>
   ) : (
     <>
-      <button onClick={hideDiagnostics}>Hide</button>
+      <Button onClick={hideDiagnostics}>Hide</Button>
+      <TableContainer
+        sx={{
+          overflow: "hidden",
+          overflowY: "scroll",
+          margin: "auto",
+        }}
+        component={Paper}
+      >
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Interval Time</TableCell>
+              <TableCell>Interval HTUs</TableCell>
+              <TableCell>Avg Time</TableCell>
+              <TableCell>T1</TableCell>
+              <TableCell>T2</TableCell>
+              <TableCell>T3</TableCell>
+              <TableCell>T4</TableCell>
+              <TableCell>T5</TableCell>
+              <TableCell>T6</TableCell>
+              <TableCell>T7</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {store.diagnostics.map((diagnostic, i) => (
+              <TableRow key={i}>
+                <TableCell scope="row">{diagnostic.interval_time.hours} hrs. {diagnostic.interval_time.minutes} min.</TableCell>
+                <TableCell scope="row">{diagnostic.interval_htu}</TableCell>
+                <TableCell scope="row">{diagnostic.avg_temp}</TableCell>
+                <TableCell scope="row">{diagnostic.t1}</TableCell>
+                <TableCell scope="row">{diagnostic.t2}</TableCell>
+                <TableCell scope="row">{diagnostic.t3}</TableCell>
+                <TableCell scope="row">{diagnostic.t4}</TableCell>
+                <TableCell scope="row">{diagnostic.t5}</TableCell>
+                <TableCell scope="row">{diagnostic.t6}</TableCell>
+                <TableCell scope="row">{diagnostic.t7}</TableCell>  
+                <TableCell scope="row">
+                  <Button>Delete</Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   );
 }
