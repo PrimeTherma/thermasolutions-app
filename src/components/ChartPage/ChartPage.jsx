bfe66d0d19b62e47d10c3d5d3a978cd0c6e2aed8
Mahmoud Mahmoud <99694897+Mahmooodyy@users.noreply.github.com>
2022-09-01 14:15:33 -0500

    stopwatch starts on page load

--- src/components/ChartPage/ChartPage.jsx
+++ src/components/ChartPage/ChartPage.jsx
@@ -1,20 +1,18 @@
-// import React, { useState } from "react";
-// import React, { Component } from "react";
+
 import "./ChartPage.css";
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import { useHistory } from "react-router-dom";
-import Stopwatch from "../Stopwatch/Stopwatch";
-// import Stopwatch from "./Stopwatch";
 import Chart from "../Chart/Chart"
+import Stopwatch from "../Stopwatch/Stopwatch";
 
 function ChartPage() {
+
   return (
     <div className="container">
       <div className="grid">
         <h3>Total HTUs: </h3>
         <Chart />
-        {/* <div id = "box"/> */}
         <Stopwatch />
       </div>
     </div>
