945729f3dfb2e2d985b8583b796bc393141f979c
Diego Baeza <diegob12@live.com>
2022-09-01 10:23:28 -0500

    linked diagnosticsRouter

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const passport = require('./strategies/user.strategy');
 // Route includes
 const userRouter = require('./routes/user.router');
 const procedureRouter = require('./routes/procedure.router');
-
+const diagnosticsRouter = require('./routes/diagnostics.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -26,6 +26,7 @@ app.use(passport.session());
 /* Routes */
 app.use('/api/user', userRouter);
 app.use('/api/procedure', procedureRouter);
+app.use('/diagnostics', diagnosticsRouter);
 
 // Serve static files
 app.use(express.static('build'));
