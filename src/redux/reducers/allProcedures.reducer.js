37214521d78dc983866e4b8417242abac475d2fd
Diego Baeza <diegob12@live.com>
2022-09-06 15:26:54 -0500

    create reducer for allProcedures

---
+++ src/redux/reducers/allProcedures.reducer.js
@@ -0,0 +1,12 @@
+const allProcedures = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_ALL_PROCEDURES':
+            return action.payload;
+        default:
+            return state;
+    }
+};
+
+// user will be on the redux state at:
+// state.user
+export default allProcedures;
