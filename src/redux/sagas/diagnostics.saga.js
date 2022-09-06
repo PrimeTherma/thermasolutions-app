42df0433805a824d4cd86d4b03e23eddfad298f4
Diego Baeza <diegob12@live.com>
2022-09-06 09:24:00 -0500

    SAGA for GET/PUT/POST

--- src/redux/sagas/diagnostics.saga.js
+++ src/redux/sagas/diagnostics.saga.js
@@ -16,6 +16,45 @@ function* getDiagnostics() {
     }
 }
 
+// Gets procedure
+function* getProcedure() {
+    console.log('in getProcedure');
+
+    try {
+        const response = yield axios.get('/api/procedure/all');
+        
+        // Stores procedure in reducer
+        yield put({type: "SET_PROCEDURE", payload: response.data});
+    } catch (error) {
+        console.log('Error GETting procedure', error);
+    }
+}
+
+// Posts diagnostics to the DB from device
+function* postDiagnostics(action) {
+    console.log('in postDiagnostics');
+    console.log('action.payload', action.payload);
+
+    try {
+        yield axios.post('/diagnostics', action.payload);
+        yield put({type: "ADD_DIAGNOSTICS"});
+    } catch (error) {
+        console.log('Error in postDiagnostics', error);
+    }
+}
+
+// Updates notes from history page
+function* editNotes(action) {
+    console.log('in editNotes, this is action.payload', action.payload);
+
+    try {
+        yield axios.put(`/api/procedure/${action.payload.id}`, action.payload);
+        yield put({type: "UPDATE_NOTES"});
+    } catch (error) {
+        console.log('Error UPDATEing notes', error);
+    }
+}
+
 // Deletes an individual diagnostic row
 function* deleteDiagnostic(action) {
     console.log('in deleteDiagnostic, this is action', action.payload);
@@ -40,24 +79,14 @@ function* deleteAllDiagnostics() {
     }
 }
 
-function* postDiagnostics(action) {
-    console.log('in postDiagnostics');
-    console.log('action.payload', action.payload);
-
-    try {
-        yield axios.post('/diagnostics', action.payload);
-        yield put({type: "ADD_DIAGNOSTICS"});
-    } catch (error) {
-        console.log('Error in postDiagnostics', error);
-    }
-}
-
 // Watches for dispatches anywhere on app
 function* diagnosticsSaga() {
     yield takeLatest("FETCH_DIAGNOSTICS", getDiagnostics);
+    yield takeLatest("FETCH_PROCEDURE", getProcedure);
     yield takeEvery("DELETE_DIAGNOSTIC", deleteDiagnostic);
     yield takeLatest("DELETE_ALL", deleteAllDiagnostics);
-    yield takeLatest("POST_DIAGNOSTICS", postDiagnostics) 
+    yield takeLatest("POST_DIAGNOSTICS", postDiagnostics);
+    yield takeLatest("EDIT_NOTES", editNotes);
 }
 
 export default diagnosticsSaga;
