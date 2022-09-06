5a94af542078cc81877fad97a709be91f595b65f
Mike Flintrop <mike.flintrop@gmail.com>
2022-09-06 10:53:48 -0500

    updated chart page to render current htus and temp

--- src/components/ChartPage/ChartPage.jsx
+++ src/components/ChartPage/ChartPage.jsx
@@ -11,13 +11,48 @@ function ChartPage() {
   const dispatch = useDispatch();
   const history = useHistory();
   const currentProcedure = useSelector((store) => store.setProcedure);
-  console.log('Miners way', currentProcedure[0]?.max);
+  const deviceDiagnostics = useSelector((store) => store.deviceDiagnostics);
+  const [currentHTUs, setCurrentHTUs] = useState(0);
+  const [currentTemp, setCurrentTemp] = useState(35);
+  // console.log('Miners way', currentProcedure[0]?.max);
 
+  const timeoutRef = useRef(null);
+
+  useEffect(() => {
+      if (timeoutRef.current !== null) {
+          
+      }
+      let interval = 2;
+      let speed = 0.1;
+      for(let i=1; i<interval; i++) {
+          timeoutRef.current = setTimeout(()=> {
+          timeoutRef.current = null;
+          fetchCurrentHTUs()
+          }, i*speed);
+      }
+  },[]);
+
+  const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+  }
+
+  const fetchCurrentHTUs = async (array) => {
+    let sum = 0;
+      for (let i=0; i<deviceDiagnostics.length; i++) {
+          await sleep (250)
+          setCurrentHTUs(sum += Number(deviceDiagnostics[i].interval_htu))
+          // setCurrentHTUs(Number(currentHTUs += deviceDiagnostics[i].interval_htu))
+          // console.log('Waiting for HTUs', currentHTUs);
+          setCurrentTemp(Number(deviceDiagnostics[i].avg_temp))
+      }
+      // return currentHTUs
+  }
 
   return (
     <div className="container">
       <div className="grid">
-        <h3>Total HTUs: </h3>
+        <h3>Total HTUs: {currentHTUs.toFixed(2)}</h3>
+        <h3>Current Temp: {currentTemp.toFixed(2)}</h3>
         <Chart />
         <Stopwatch />
       </div>
